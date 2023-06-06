877913cbdd109c86bf05c2c02d3a6cf5c97fb8ec
wiktor <wiktortomczyk1422@gmail.com>
2023-06-06 14:13:22 +0200

    fixed save dialog

--- SaveDialog.py
+++ SaveDialog.py
@@ -1,12 +1,3 @@
-# -*- coding: utf-8 -*-
-
-################################################################################
-## Form generated from reading UI file 'saving.ui'
-##
-## Created by: Qt User Interface Compiler version 6.5.1
-##
-## WARNING! All changes made in this file will be lost when recompiling UI file!
-################################################################################
 
 from PySide6.QtCore import (QCoreApplication, QMetaObject, QRect,
                             Qt)
